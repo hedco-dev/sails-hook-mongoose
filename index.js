2635c6dc9e717f32bf93d85d1da3a8dead52f34a
hossein derakhshan <derakhshan.hossein@hedcoint.com>
2016-03-05 14:35:35 +0330

    initialze

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = function(sails) {
+
+  let async             = require('async');
+  let utils             = require('./utils.js');
+  let mongoose          = require('mongoose');
+  let path              = require('path');
+  let db                = mongoose.connection;
+  let Schema            = global.Schema = mongoose.Schema;
+  Schema.createObjectId = function() {
+    return mongoose.Types.ObjectId();
+  };
+
+  Schema.castToObjectId = function(stringObjectid) {
+    return mongoose.Types.ObjectId(stringObjectid);
+  };
+
+  mongoose.Promise  = require('bluebird');
+
+  let hook          = {
+    defaults: {
+
+      models: {
+        connection: 'localMongoDb'
+      },
+
+      connections: {
+        localMongoDb: {
+          // url: 'mongodb://localhost:27017/testDb'
+        }
+      }
+    },
+    configure: function() {
+      sails.once('lower', hook.teardown);
+    },
+    initialize: function(cb) {
+      var modelPath = path.join(sails.config.appPath, 'api', 'models');
+      let modelDefinitions = utils.loadModules(modelPath);
+
+      if(!sails.models) {
+        sails.models = {};
+      }
+
+      utils.clean(mongoose);
+
+      async.forEachOf(modelDefinitions, function(modelDefinition, modelName, next){
+        global[modelName] = sails.models[modelName] = utils.createModel(modelDefinition, modelName, mongoose, Schema);
+        next();
+      });
+
+      if (sails.config.connections && sails.config.models &&
+        sails.config.models.connection &&
+        sails.config.connections[sails.config.models.connection].url) {
+        mongoose.connect(sails.config.connections[sails.config.models.connection].url);
+        db.on('error', function(err){
+          cb(err);
+        });
+
+        db.once('open', function () {
+          cb();
+        });
+      } else {
+        cb(new Error('Can not find a connection to the mongo db'));
+      }
+    },
+    teardown: function() {
+      db.close();
+    }
+  };
+
+  return hook;
+};
---
+++ utils.js
@@ -0,0 +1,70 @@
+'use strict';
+let timestamps = require('mongoose-timestamp');
+let includeAll = require('include-all');
+
+let loadModules = function(relPath) {
+  return includeAll({
+    dirname: require('path').resolve(__dirname, relPath),
+    filter: /(.+)\.js$/
+  }) || {};
+};
+
+// let createdbyplugin = function(schema, options) {
+//   schema.add({
+//     createdUser: String
+//   });
+//
+//   schema.pre('save', function(next) {
+//     // this.createdUser = helpdeskNamespace.get('userId');
+//     next();
+//   });
+// };
+
+let createModel = function(modelDefinition, modelName, mongoose, Schema) {
+  let modelSchema = Schema(modelDefinition.attributes, {autoIndex: false});
+
+  modelSchema.methods.toJSON = function() {
+    let obj = this.toObject();
+    delete obj.__v;
+    return obj;
+  };
+
+  for (let methodName in modelDefinition.methods) {
+    let method = modelDefinition.methods[methodName];
+    modelSchema.methods[methodName] = method;
+  }
+
+  for (let methodName in modelDefinition.statics) {
+    let method = modelDefinition.statics[methodName];
+    modelSchema.statics[methodName] = method;
+  }
+
+  modelSchema.plugin(timestamps);
+  // modelSchema.plugin(createdbyplugin);
+
+  let model = mongoose.model(modelName, modelSchema);
+
+  for(let propertyName in modelDefinition) {
+    if(propertyName !== 'attributes' && propertyName !== 'methods' && propertyName !== 'statics') {
+      model[propertyName] = modelDefinition[propertyName];
+    }
+  }
+
+  return model;
+};
+
+function clean(mongoose){
+  for(let modelName in mongoose.models) {
+    delete mongoose.models[modelName];
+  }
+
+  for(let schemaName in mongoose.modelSchemas) {
+    delete mongoose.modelSchemas[schemaName];
+  }
+}
+
+module.exports = {
+  loadModules,
+  createModel,
+  clean
+};
